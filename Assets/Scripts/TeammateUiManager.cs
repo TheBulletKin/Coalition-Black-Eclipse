46794388dde0edea8f735dfd13f7c49c528bec79
Oliver Warry <109860317+TheBulletKin@users.noreply.github.com>
2024-12-11 20:18:10 +0000

    Teammate nameplates
    Colour coded and numbered nameplates follow teammates and show on the UI

--- Assets/Scripts/TeammateUiManager.cs
+++ Assets/Scripts/TeammateUiManager.cs
@@ -1,12 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class TeammateUiManager : MonoBehaviour
 {
 	public PlayerCommandIssuer player;
 	public Dictionary<AiCommandListener, UiTeammateCard> teammateToUiCard = new Dictionary<AiCommandListener, UiTeammateCard>();
+	public Dictionary<AiCommandListener, UiTeammateNameplate> teammateToUiTemplate = new Dictionary<AiCommandListener, UiTeammateNameplate>();
 	public GameObject teammateUiCardPrefab;
+	public GameObject teammateUiTemplatePrefab;
+	public RectTransform nameplatePanel;
+	public float nameplateHeightOffset = 2f;
 
 	private void Start()
 	{
@@ -21,6 +26,13 @@ public class TeammateUiManager : MonoBehaviour
 				teammateCard.background.color = ai.teammateColour;
 				teammateCard.teamIndexText.text = aiTeammates.IndexOf(ai).ToString();
 				teammateToUiCard.Add(ai, teammateCard);
+
+				GameObject newTeammateNameplateObject = Instantiate(teammateUiTemplatePrefab, gameObject.transform.position, gameObject.transform.rotation, nameplatePanel);
+				UiTeammateNameplate teammateNameplate = newTeammateNameplateObject.GetComponent<UiTeammateNameplate>();
+				teammateNameplate.teammateColour = ai.teammateColour;
+				teammateNameplate.background.color = ai.teammateColour;
+				teammateNameplate.teamIndexText.text = aiTeammates.IndexOf(ai).ToString();
+				teammateToUiTemplate.Add(ai, teammateNameplate);
 			}
 
 			player.OnTeammateOrGroupChanged += SwitchActiveCard;
@@ -50,8 +62,32 @@ public class TeammateUiManager : MonoBehaviour
 			UiTeammateCard activeCard = teammateToUiCard[player.GetTeammates()[teammateOrGroupIndex]];
 			activeCard.ToggleAsActiveTeammate(true);
 		}
-		
 
+
+
+	}
+
+	private void Update()
+	{
+
+		foreach (KeyValuePair<AiCommandListener, UiTeammateNameplate> pair in teammateToUiTemplate)
+		{
+			Vector3 screenPosition = Camera.main.WorldToScreenPoint(pair.Key.transform.position + Vector3.up * nameplateHeightOffset);
+			screenPosition.x = Mathf.Clamp(screenPosition.x, 0, Screen.width);
+			screenPosition.y = Mathf.Clamp(screenPosition.y, 0, Screen.height);
+
+			
+			if (screenPosition.z < 0)
+			{
+				pair.Value.gameObject.SetActive(false);
+			}
+			else
+			{
+				pair.Value.gameObject.SetActive(true);				
+				pair.Value.transform.position = screenPosition;
+			}
+		}
+		
 
 	}
 }
---
+++ Assets/Scripts/UiTeammateNameplate.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UiTeammateNameplate : MonoBehaviour
+{
+	public TextMeshProUGUI teamIndexText;
+	public Color teammateColour { set; private get; }
+	public Image background;
+}
